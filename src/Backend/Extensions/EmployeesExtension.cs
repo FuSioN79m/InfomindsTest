d2da68e7fefb9442607dc6b5d407160fdd4f173e
FuSioN79m <michele.picaro@gmail.com>
2025-01-14 18:10:24 +0100

    Employee API optimizations

---
+++ src/Backend/Extensions/EmployeesExtension.cs
@@ -0,0 +1,33 @@
+﻿using Backend.Features.Employees;
+
+namespace Backend.Extensions
+{
+	public static class EmployeesExtension
+	{
+		public static EmployeesListQueryResponse toEmployeesDto(this Employee c)
+		{
+			return new EmployeesListQueryResponse()
+			{
+				Id = c.Id,
+				Address = c.Address,
+				Code = c.Code,
+				Department = c.Department == null ? null : new EmployeesListQueryResponseDepartment() { Code =  c.Department.Code, Description = c.Department.Description },
+				Email = c.Email,
+				FirstName = c.FirstName,
+				LastName = c.LastName,
+				Phone = c.Phone			
+
+			};
+		}
+	}
+}
+/*
+     public int Id { get; set; }
+    public string Code { get; internal set; } = "";
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public string Address { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Phone { get; set; } = "";
+    public EmployeesListQueryResponseDepartment? Department { get; set; }
+ */
--- src/Backend/Features/Employees/EmployeesListQuery.cs
+++ src/Backend/Features/Employees/EmployeesListQuery.cs
@@ -1,70 +1,77 @@
+using Backend.Extensions;
+
 namespace Backend.Features.Employees;
 
 public class EmployeesListQuery : IRequest<List<EmployeesListQueryResponse>>
 {
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
+	public string? FirstName { get; set; }
+	public string? LastName { get; set; }
 }
 
 public class EmployeesListQueryResponse
 {
-    public int Id { get; set; }
-    public string Code { get; internal set; } = "";
-    public string FirstName { get; set; } = "";
-    public string LastName { get; set; } = "";
-    public string Address { get; set; } = "";
-    public string Email { get; set; } = "";
-    public string Phone { get; set; } = "";
-    public EmployeesListQueryResponseDepartment? Department { get; set; }
+	public int Id { get; set; }
+	public string Code { get; internal set; } = "";
+	public string FirstName { get; set; } = "";
+	public string LastName { get; set; } = "";
+	public string Address { get; set; } = "";
+	public string Email { get; set; } = "";
+	public string Phone { get; set; } = "";
+	public EmployeesListQueryResponseDepartment? Department { get; set; }
 }
 
 public class EmployeesListQueryResponseDepartment
 {
-    public string Code { get; set; } = "";
-    public string Description { get; set; } = "";
+	public string Code { get; set; } = "";
+	public string Description { get; set; } = "";
 }
 
 
 internal class EmployeesListQueryHandler(BackendContext context) : IRequestHandler<EmployeesListQuery, List<EmployeesListQueryResponse>>
 {
-    private readonly BackendContext context = context;
+	private readonly BackendContext context = context;
 
-    public async Task<List<EmployeesListQueryResponse>> Handle(EmployeesListQuery request, CancellationToken cancellationToken)
-    {
-        var query = context.Employees.AsQueryable();
-        if (!string.IsNullOrEmpty(request.FirstName))
-            query = query.Where(q => q.FirstName.ToLower().Contains(request.FirstName.ToLower()));
-        if (!string.IsNullOrEmpty(request.LastName))
-            query = query.Where(q => q.LastName.ToLower().Contains(request.LastName.ToLower()));
+	public async Task<List<EmployeesListQueryResponse>> Handle(EmployeesListQuery request, CancellationToken cancellationToken)
+	{
+		//including Department to execute one single query
+		var query = context.Employees.Include(t => t.Department).AsQueryable();
+		if (!string.IsNullOrEmpty(request.FirstName))
+			query = query.Where(q => q.FirstName.ToLower().Contains(request.FirstName.ToLower()));
+		if (!string.IsNullOrEmpty(request.LastName))
+			query = query.Where(q => q.LastName.ToLower().Contains(request.LastName.ToLower()));
 
-        var data = await query.OrderBy(q => q.LastName).ThenBy(q => q.FirstName).ToListAsync(cancellationToken);
-        var result = new List<EmployeesListQueryResponse>();
-
-        foreach (var item in data)
-        {
-            var resultItem = new EmployeesListQueryResponse
-            {
-                Id = item.Id,
-                Code = item.Code,
-                FirstName = item.FirstName,
-                LastName = item.LastName,
-                Address = item.Address,
-                Email = item.Email,
-                Phone = item.Phone,
-            };
-
-            var department = await context.Departments.SingleOrDefaultAsync(q => q.Id == item.DepartmentId, cancellationToken);
-            if (department is not null)
-                resultItem.Department = new EmployeesListQueryResponseDepartment
-                {
-                    Code = department.Code,
-                    Description = department.Description
-                };
+		var data = await query.OrderBy(q => q.LastName).ThenBy(q => q.FirstName).ToListAsync(cancellationToken);
+		var result = new List<EmployeesListQueryResponse>();
+		foreach (var item in data)
+			result.Add(item.toEmployeesDto());//fill object using extension method
 
 
-            result.Add(resultItem);
-        }
+		//old code
+		//foreach (var item in data)
+		//      {
+		//          var resultItem = new EmployeesListQueryResponse
+		//          {
+		//              Id = item.Id,
+		//              Code = item.Code,
+		//              FirstName = item.FirstName,
+		//              LastName = item.LastName,
+		//              Address = item.Address,
+		//              Email = item.Email,
+		//              Phone = item.Phone,
+		//          };
 
-        return result;
-    }
+		//          var department = await context.Departments.SingleOrDefaultAsync(q => q.Id == item.DepartmentId, cancellationToken);
+		//          if (department is not null)
+		//              resultItem.Department = new EmployeesListQueryResponseDepartment
+		//              {
+		//                  Code = department.Code,
+		//                  Description = department.Description
+		//              };
+
+
+		//          result.Add(resultItem);
+		//      }
+
+		return result;
+	}
 }
